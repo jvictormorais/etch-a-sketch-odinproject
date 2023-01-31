0e0b0e8ab68ef04a9379d083b8df4ffafa3cc971
jvictormorais <jose.alvarezmorais@gmail.com>
2023-01-31 18:20:43 -0300

    Do a etch-a-sketch

---
+++ index.js
@@ -0,0 +1,47 @@
+let color = "black";
+
+function populateBoard(size){
+    let board = document.querySelector(".board");
+    let square=board.querySelectorAll("div");
+    square.forEach((div)=>div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size
+    for (let i=0; i<amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend",square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >=2 && input <=100){
+        populateBoard(input);
+    }
+    else{
+        console.log("Size must be between 2 and 100!");
+    }
+}
+
+function colorSquare(){
+    if((color === "random")){
+        this.style.backgroundColor= `hsl(${Math.random()*360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice){
+    color=choice;
+
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let square=board.querySelectorAll("div");
+    square.forEach((div)=>div.style.backgroundColor = "white")
+}
